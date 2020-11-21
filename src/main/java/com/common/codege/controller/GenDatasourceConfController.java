4a653b9719d46ff3d1dbdc98d806b779ab8f1f30
wangchao5505 <550586585@qq.com>
2020-11-21 19:52:53 +0800

    添加页面支持

--- src/main/java/com/common/codege/controller/GenDatasourceConfController.java
+++ src/main/java/com/common/codege/controller/GenDatasourceConfController.java
@@ -9,6 +9,8 @@ import com.common.codege.service.GenDatasourceConfService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
  * @author wangchao
  * @date 2020-11-19 11:17:34
  */
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/gendatasourceconf" )
 @Api(value = "gendatasourceconf", tags = "数据源表管理")
@@ -34,8 +36,9 @@ public class GenDatasourceConfController {
      */
     @ApiOperation(value = "分页查询", notes = "分页查询")
     @GetMapping("/page" )
-    public R getGenDatasourceConfPage(Page page, GenDatasourceConf genDatasourceConf) {
-        return R.ok(genDatasourceConfService.page(page, Wrappers.query(genDatasourceConf)));
+    public String getGenDatasourceConfPage(Model model, Page page, GenDatasourceConf genDatasourceConf) {
+        model.addAttribute("genDatasourceConfList",genDatasourceConfService.page(page, Wrappers.query(genDatasourceConf)));
+        return "/index";
     }
 
 
@@ -46,6 +49,7 @@ public class GenDatasourceConfController {
      */
     @ApiOperation(value = "通过id查询", notes = "通过id查询")
     @GetMapping("/{id}" )
+    @ResponseBody
     public R getById(@PathVariable("id" ) Integer id) {
         return R.ok(genDatasourceConfService.getById(id));
     }
@@ -57,6 +61,7 @@ public class GenDatasourceConfController {
      */
     @ApiOperation(value = "新增数据源表", notes = "新增数据源表")
     @PostMapping
+    @ResponseBody
     public R save(@RequestBody GenDatasourceConf genDatasourceConf) {
         return R.ok(genDatasourceConfService.save(genDatasourceConf));
     }
@@ -68,6 +73,7 @@ public class GenDatasourceConfController {
      */
     @ApiOperation(value = "修改数据源表", notes = "修改数据源表")
     @PutMapping
+    @ResponseBody
     public R updateById(@RequestBody GenDatasourceConf genDatasourceConf) {
         return R.ok(genDatasourceConfService.updateById(genDatasourceConf));
     }
@@ -79,6 +85,7 @@ public class GenDatasourceConfController {
      */
     @ApiOperation(value = "通过id删除数据源表", notes = "通过id删除数据源表")
     @DeleteMapping("/{id}" )
+    @ResponseBody
     public R removeById(@PathVariable Integer id) {
         return R.ok(genDatasourceConfService.removeById(id));
     }
--- src/main/java/com/common/codege/service/impl/GeneratorServiceImpl.java
+++ src/main/java/com/common/codege/service/impl/GeneratorServiceImpl.java
@@ -5,7 +5,7 @@ import cn.hutool.core.io.IoUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.common.codege.datasource.DataSourceBean;
-import com.common.codege.datasource.DynamicDataSource;
+import com.common.codege.datasource.DynamicDataSource.DataSourceContext;
 import com.common.codege.entity.ColumnEntity;
 import com.common.codege.entity.GenDatasourceConf;
 import com.common.codege.entity.GeneratorConf;
@@ -41,18 +41,32 @@ import java.util.zip.ZipOutputStream;
 public class GeneratorServiceImpl implements GeneratorService {
     private final GeneratorMapper generatorMapper;
     private final GenDatasourceConfService genDatasourceConfService;
-    private static String path = GeneratorServiceImpl.class.getClass().getResource("/ftl").getPath();
+    private static String path = GeneratorServiceImpl.class.getClass().getResource("/template").getPath();
     private static final Pattern pattern = Pattern.compile("([a-z])([a-z]*)", Pattern.CASE_INSENSITIVE);
+
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            InputStream in = GeneratorServiceImpl.class.getClassLoader().getResourceAsStream("generator.properties");
+            properties.load(in);
+        } catch (IOException e) {
+        }
+    }
+
+
     @Override
     public IPage<List<Map<String, Object>>> page(Page page, String tableName, String dsName) {
         GenDatasourceConf genDatasourceConf = genDatasourceConfService.lambdaQuery().in(GenDatasourceConf::getName, dsName).one();
-
-        DataSourceBean dataSourceBean = new DataSourceBean(genDatasourceConf.getName(), genDatasourceConf.getUrl(), genDatasourceConf.getUsername(), genDatasourceConf.getPassword());
-        DynamicDataSource.DataSourceContext.setDataSource(dataSourceBean);
+        DataSourceBean dataSourceBean = new DataSourceBean(genDatasourceConf.getName(),
+                genDatasourceConf.getUrl(),
+                genDatasourceConf.getUsername(),
+                genDatasourceConf.getPassword());
+        DataSourceContext.setDataSource(dataSourceBean);
 
         IPage<List<Map<String, Object>>> list = generatorMapper.list(page, tableName);
 
-        DynamicDataSource.DataSourceContext.toDefault();
+        DataSourceContext.toDefault();
         return list;
     }
 
@@ -69,9 +83,6 @@ public class GeneratorServiceImpl implements GeneratorService {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ZipOutputStream zip = new ZipOutputStream(outputStream);
         genFile(new File(path), root, cfg, zip);
-//        byte[] bytes = outputStream.toByteArray();
-//        IoUtil.close(outputStream);
-
         IoUtil.close(zip);
         return outputStream.toByteArray();
     }
@@ -87,25 +98,34 @@ public class GeneratorServiceImpl implements GeneratorService {
             String s = fileName.split("\\.")[0];
             String genFileName = fileName.equals("Entity.java.ftl") ? ".java" : fileName.replaceAll(".ftl", "");
 
-            String path = root.getPackageName().concat("." + s.toLowerCase());
-            path = path.replace(".", File.separator).concat(File.separator + root.getClassName() + genFileName);
-            path = path.replace("serviceimpl", "service" + File.separator + "impl");
+            //构造文件路径
+            String filePath = root.getPackageName().concat("." + s.toLowerCase());
+            filePath = filePath.replace(".", File.separator).concat(File.separator + root.getClassName() + genFileName);
+            filePath = filePath.replace("serviceimpl", "service" + File.separator + "impl");
+
             //获取模板
             Template temp = cfg.getTemplate(fileName);
-            //生成文件
-            StringWriter sw = new StringWriter();
-            temp.process(root, sw);
 
-            temp.process(root, new FileWriter(new File("D:/mywork/新建文件夹/roseWood/yokea-provider/yokea-pro/yokea-pro-crm/src/main/java/com/yokea/pro/crm/feign/" + genFileName).getCanonicalFile()));
-
-            zip.putNextEntry(new ZipEntry(Objects.requireNonNull(path)));
-            IoUtil.write(zip, StandardCharsets.UTF_8, false, sw.toString());
-            IoUtil.close(sw);
-            zip.closeEntry();
-
+            //生成zip
+            genZip(root, zip, filePath, temp);
         }
     }
 
+    private void genZip(TableEntity root, ZipOutputStream zip, String path, Template temp) throws TemplateException, IOException {
+        StringWriter sw = new StringWriter();
+        temp.process(root, sw);
+        zip.putNextEntry(new ZipEntry(Objects.requireNonNull(path)));
+        IoUtil.write(zip, StandardCharsets.UTF_8, false, sw.toString());
+
+        IoUtil.close(sw);
+        zip.closeEntry();
+    }
+
+    /**
+     * 获取模板配置
+     * @return
+     * @throws IOException
+     */
     private Configuration getConfiguration() throws IOException {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
         cfg.setDirectoryForTemplateLoading(new File(path));
@@ -116,55 +136,74 @@ public class GeneratorServiceImpl implements GeneratorService {
 
     @SneakyThrows
     private TableEntity getRoot(GeneratorConf generatorConf) {
-        Properties properties = new Properties();
-        InputStream in = GeneratorServiceImpl.class.getClassLoader().getResourceAsStream("generator.properties");
-        properties.load(in);
-
-        TableEntity root = new TableEntity();
-        root.setAuthor(Objects.isNull(generatorConf.getAuthor()) ? properties.getProperty("author") : generatorConf.getAuthor());
-        root.setPackageName(Objects.isNull(generatorConf.getPackageName()) ? properties.getProperty("packageName") : generatorConf.getPackageName());
-        root.setDatetime(DateUtil.now());
-
         //切换数据源
         String dsName = generatorConf.getDsName();
         GenDatasourceConf genDatasourceConf = genDatasourceConfService.lambdaQuery().in(GenDatasourceConf::getName, dsName).one();
         DataSourceBean dataSourceBean = new DataSourceBean(genDatasourceConf.getName(), genDatasourceConf.getUrl(), genDatasourceConf.getUsername(), genDatasourceConf.getPassword());
-        DynamicDataSource.DataSourceContext.setDataSource(dataSourceBean);
+        DataSourceContext.setDataSource(dataSourceBean);
 
-
-        //解析表
+        //获取表相关数据
         List<Map<String, Object>> tables = generatorMapper.getTableByTableName(generatorConf.getTableName());
 
-        //解析列
+        //获取列相关数据
         List<Map<String, Object>> columns = generatorMapper.getColumnByTableName(generatorConf.getTableName());
 
         //恢复默认数据源
-        DynamicDataSource.DataSourceContext.toDefault();
+        DataSourceContext.toDefault();
 
+        //设置table
+        TableEntity root = setTable(generatorConf, tables);
+
+        //设置列
+        List<ColumnEntity> columnEntities = setColumn(properties, columns);
+        root.setCols(columnEntities);
+
+        return root;
+    }
+
+    private TableEntity setTable(GeneratorConf generatorConf, List<Map<String, Object>> tables) {
+        TableEntity root = new TableEntity();
+        root.setAuthor(Objects.isNull(generatorConf.getAuthor()) ? properties.getProperty("author") : generatorConf.getAuthor());
+        root.setPackageName(Objects.isNull(generatorConf.getPackageName()) ? properties.getProperty("packageName") : generatorConf.getPackageName());
+        root.setDatetime(DateUtil.now());
         root.setTableName(String.valueOf(tables.get(0).get("tableName")));
         root.setTableComment(String.valueOf(tables.get(0).get("tableComment")));
         root.setClassName(toV(String.valueOf(tables.get(0).get("tableName")), false));
         root.setUrl(root.getClassName().toLowerCase());
         root.setClassVarName(toV(String.valueOf(tables.get(0).get("tableName")), true));
-        List<ColumnEntity> cols = new ArrayList<>();
+        return root;
+    }
 
+    /**
+     * 设置字段
+     *
+     * @param properties
+     * @param columns
+     * @return
+     */
+    private List<ColumnEntity> setColumn(Properties properties, List<Map<String, Object>> columns) {
+        List<ColumnEntity> cols = new ArrayList<>();
         columns.forEach(col -> {
             ColumnEntity cols1 = new ColumnEntity();
             //驼峰命名小写
             cols1.setFieldName(toV(String.valueOf(col.get("colName")), true));
             cols1.setColName(String.valueOf(col.get("colName")));
-
             cols1.setDataType(properties.getProperty(String.valueOf(col.get("dataType"))));
-
             cols1.setKeyType(String.valueOf(col.get("keyType")));
             cols1.setColComment(String.valueOf(col.get("colComment")));
             cols1.setKeyStatus(String.valueOf(col.get("keyStatus")));
             cols.add(cols1);
         });
-        root.setCols(cols);
-        return root;
+        return cols;
     }
 
+    /**
+     * 转换驼峰命名
+     *
+     * @param tableName
+     * @param flag      首字母是否小写
+     * @return
+     */
     private static String toV(String tableName, boolean flag) {
         StringBuffer stringbf = new StringBuffer();
         Matcher m = pattern.matcher(tableName);
